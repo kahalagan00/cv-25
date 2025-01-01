0954c0498b0000c6d5ddd30cf19224c5178c5b6b
kahalagan00 <privatelampas@gmail.com>
2025-01-01 13:18:19 -0800

    Continue to work on the header

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,28 +1,86 @@
+"use client";
+
 import { Rubik } from "next/font/google";
 import Link from "next/link";
+import { useState } from "react";
+import ThemeMenu from "./ThemeMenu";
 
 const rubik = Rubik({ weight: "400", subsets: ["latin"] });
 
 const Header: React.FC = () => {
+  const [linkSelected, setLinkSelected] = useState("home");
+
+  {
+    /* Home | Projects | About | Resources | Contact | Theme */
+  }
   return (
-    <div className="mx-auto mt-4 flex h-20 max-w-screen-xl items-center justify-between rounded-full border-2 border-violet-600 bg-indigo-950 px-8 text-white">
-      <div className="text-3xl font-semibold">Joshmar Morales</div>
-      <Link href="/" className={`${rubik.className} text-sm`}>
-        Home
-      </Link>
-      <Link href="/projects" className={`${rubik.className} text-sm`}>
-        Projects
-      </Link>
-      <Link href="/about" className={`${rubik.className} text-sm`}>
-        About
-      </Link>
-      <Link href="/resources" className={`${rubik.className} text-sm`}>
-        Resources
-      </Link>
-      <div className={`${rubik.className} text-sm`}>Theme</div>
-      <Link href="/contact" className={`${rubik.className} text-sm`}>
-        Contact
-      </Link>
+    <div className="mx-auto mt-4 flex h-20 max-w-screen-xl items-center justify-start gap-x-8 rounded-full border-2 border-violet-800 bg-indigo-950 pl-8 text-white">
+      <div className="font-clashregular text-3xl font-semibold">
+        Joshmar Morales
+      </div>
+      <div className="relative h-full w-28">
+        <Link
+          onClick={() => setLinkSelected("home")}
+          href="/"
+          className={`${rubik.className} absolute left-1/2 top-6 -translate-x-1/2 transform text-lg`}
+        >
+          Home
+        </Link>
+        {linkSelected === "home" && (
+          <div className="absolute -bottom-1 left-1/2 h-2 w-10 -translate-x-1/2 transform rounded-full bg-fuchsia-600"></div>
+        )}
+      </div>
+      <div className="relative h-full w-28">
+        <Link
+          onClick={() => setLinkSelected("projects")}
+          href="/projects"
+          className={`${rubik.className} absolute left-1/2 top-6 -translate-x-1/2 transform text-lg`}
+        >
+          Projects
+        </Link>
+        {linkSelected === "projects" && (
+          <div className="absolute -bottom-1 left-1/2 h-2 w-10 -translate-x-1/2 transform rounded-full bg-fuchsia-600"></div>
+        )}
+      </div>
+
+      <div className="relative h-full w-28">
+        <Link
+          onClick={() => setLinkSelected("about")}
+          href="/about"
+          className={`${rubik.className} absolute left-1/2 top-6 -translate-x-1/2 transform text-lg`}
+        >
+          About
+        </Link>
+        {linkSelected === "about" && (
+          <div className="absolute -bottom-1 left-1/2 h-2 w-10 -translate-x-1/2 transform rounded-full bg-fuchsia-600"></div>
+        )}
+      </div>
+
+      <div className="relative h-full w-28">
+        <Link
+          onClick={() => setLinkSelected("resources")}
+          href="/resources"
+          className={`${rubik.className} absolute left-1/2 top-6 -translate-x-1/2 transform text-lg`}
+        >
+          Resources
+        </Link>
+        {linkSelected === "resources" && (
+          <div className="absolute -bottom-1 left-1/2 h-2 w-10 -translate-x-1/2 transform rounded-full bg-fuchsia-600"></div>
+        )}
+      </div>
+
+      <div>
+        <ThemeMenu />
+      </div>
+      <div className="ml-auto flex h-full w-48 items-center justify-center">
+        <Link
+          onClick={() => setLinkSelected("contact")}
+          href="/contact"
+          className={`${rubik.className} rounded-full bg-violet-900 px-9 py-4 text-lg hover:bg-violet-800`}
+        >
+          Contact me
+        </Link>
+      </div>
     </div>
   );
 };
---
+++ src/app/_components/ThemeMenu.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+// import toast from "react-hot-toast";
+// import { IoMdHeartEmpty, IoMdMoon, IoMdSunny } from "react-icons/io";
+import { FiMoon, FiSun } from "react-icons/fi";
+
+const ThemeMenu: React.FC = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  // Load theme from localStorage on mount
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark") {
+      document.documentElement.classList.add("dark");
+      setIsDarkMode(true);
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, []);
+
+  // Toggle dark mode
+  const handleClick = () => {
+    if (isDarkMode) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+      setIsDarkMode(false);
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      setIsDarkMode(true);
+    }
+  };
+
+  return (
+    <button onClick={handleClick}>
+      {!isDarkMode ? (
+        <FiSun className="h-10 w-10 rounded-full p-2 hover:bg-violet-500" />
+      ) : (
+        <FiMoon className="h-10 w-10 rounded-full p-2 hover:bg-violet-500" />
+      )}
+    </button>
+  );
+};
+
+export default ThemeMenu;
