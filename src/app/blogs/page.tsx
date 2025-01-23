a752ee3b3a8e982f24b3cf3120e256f8e052ee84
kahalagan00 <privatelampas@gmail.com>
2025-01-22 17:56:21 -0800

    Fixed Blogs page footer

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -9,15 +9,16 @@ import MainFooter from "../_components/MainFooter";
 const Page: React.FC = () => {
   return (
     <section className="relative w-screen">
-      <div className="relative z-10 mx-auto my-16 w-full max-w-screen-2xl px-4">
-        <h1 className="mb-16 font-clashsemibold text-[4rem] leading-tight text-white sm:text-[6rem] xl:text-[8rem]">
-          Blogs
-        </h1>
-        <p className="font-clashlight text-[1.5rem] text-white sm:text-[2rem] lg:text-[2.5rem]">
-          Blogs coming soon...
-        </p>
-
-        <div className="mt-32">
+      <div className="relative z-10 mx-auto my-16 flex min-h-screen w-full max-w-screen-2xl flex-col justify-between px-4">
+        <div>
+          <h1 className="mb-16 font-clashsemibold text-[4rem] leading-tight text-white sm:text-[6rem] xl:text-[8rem]">
+            Blogs
+          </h1>
+          <p className="font-clashlight text-[1.5rem] text-white sm:text-[2rem] lg:text-[2.5rem]">
+            Blogs coming soon...
+          </p>
+        </div>
+        <div className="">
           <MainFooter />
         </div>
       </div>
