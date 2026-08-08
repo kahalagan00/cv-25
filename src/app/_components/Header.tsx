bd1534a14432e2ad9c4a1ae5ed3f972e195e8ab3
kahalagan00 <privatelampas@gmail.com>
2026-08-08 16:50:27 -0700

    Created a single underline that moves smoothly across the header menu links

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -10,8 +10,12 @@ import { FaHouse } from "react-icons/fa6";
 const rubik = Rubik({ weight: "400", subsets: ["latin"] });
 const rubikBold = Rubik({ weight: "700", subsets: ["latin"] });
 
+// Order of the underlined nav items; drives the sliding underline's position.
+const navOrder = ["home", "about", "projects", "resources"];
+
 const Header: React.FC = () => {
   const { currentLink, setCurrentLink } = useLinkContext();
+  const activeIndex = navOrder.indexOf(currentLink);
 
   return (
     <div className="mx-auto mt-4 flex h-20 max-w-screen-xl items-center justify-start gap-x-8 rounded-full border-2 border-violet-800 bg-indigo-950 pl-8 text-white">
@@ -31,88 +35,87 @@ const Header: React.FC = () => {
           JM
         </Link>
       </div>
-      <div className="relative h-full w-28">
-        <Link
-          onClick={() => setCurrentLink("home")}
-          href="/"
-          className={`${rubik.className} absolute left-1/2 top-6 hidden -translate-x-1/2 transform text-lg lg:block`}
-        >
-          Home
-        </Link>
-        <Link
-          title="Go back to homepage"
-          onClick={() => setCurrentLink("home")}
-          href="/"
-          className={`${rubik.className} absolute left-1/2 top-6 -translate-x-1/2 transform text-lg lg:hidden`}
-        >
-          <FaHouse />
-        </Link>
-        {currentLink === "home" && (
-          <div className="absolute -bottom-1 left-1/2 h-2 w-10 -translate-x-1/2 transform rounded-full bg-fuchsia-600"></div>
-        )}
-      </div>
-      <div className="relative h-full w-28">
-        <Link
-          onClick={() => setCurrentLink("about")}
-          href="/about"
-          className={`${rubik.className} absolute left-1/2 top-6 hidden -translate-x-1/2 transform text-lg lg:block`}
-        >
-          About
-        </Link>
+      <div className="relative flex h-full gap-x-8">
+        <div className="relative h-full w-28">
+          <Link
+            onClick={() => setCurrentLink("home")}
+            href="/"
+            className={`${rubik.className} absolute left-1/2 top-6 hidden -translate-x-1/2 transform text-lg lg:block`}
+          >
+            Home
+          </Link>
+          <Link
+            title="Go back to homepage"
+            onClick={() => setCurrentLink("home")}
+            href="/"
+            className={`${rubik.className} absolute left-1/2 top-6 -translate-x-1/2 transform text-lg lg:hidden`}
+          >
+            <FaHouse />
+          </Link>
+        </div>
+        <div className="relative h-full w-28">
+          <Link
+            onClick={() => setCurrentLink("about")}
+            href="/about"
+            className={`${rubik.className} absolute left-1/2 top-6 hidden -translate-x-1/2 transform text-lg lg:block`}
+          >
+            About
+          </Link>
 
-        <Link
-          title="See more about me"
-          onClick={() => setCurrentLink("about")}
-          href="/about"
-          className={`${rubik.className} absolute left-1/2 top-6 -translate-x-1/2 transform text-lg lg:hidden`}
-        >
-          <FaUser />
-        </Link>
-        {currentLink === "about" && (
-          <div className="absolute -bottom-1 left-1/2 h-2 w-10 -translate-x-1/2 transform rounded-full bg-fuchsia-600"></div>
-        )}
-      </div>
-      <div className="relative h-full w-28">
-        <Link
-          onClick={() => setCurrentLink("projects")}
-          href="/projects"
-          className={`${rubik.className} absolute left-1/2 top-6 hidden -translate-x-1/2 transform text-lg lg:block`}
-        >
-          Projects
-        </Link>
-        <Link
-          title="See projects"
-          onClick={() => setCurrentLink("projects")}
-          href="/projects"
-          className={`${rubik.className} absolute left-1/2 top-6 -translate-x-1/2 transform text-lg lg:hidden`}
-        >
-          <FaBuilding />
-        </Link>
-        {currentLink === "projects" && (
-          <div className="absolute -bottom-1 left-1/2 h-2 w-10 -translate-x-1/2 transform rounded-full bg-fuchsia-600"></div>
-        )}
-      </div>
+          <Link
+            title="See more about me"
+            onClick={() => setCurrentLink("about")}
+            href="/about"
+            className={`${rubik.className} absolute left-1/2 top-6 -translate-x-1/2 transform text-lg lg:hidden`}
+          >
+            <FaUser />
+          </Link>
+        </div>
+        <div className="relative h-full w-28">
+          <Link
+            onClick={() => setCurrentLink("projects")}
+            href="/projects"
+            className={`${rubik.className} absolute left-1/2 top-6 hidden -translate-x-1/2 transform text-lg lg:block`}
+          >
+            Projects
+          </Link>
+          <Link
+            title="See projects"
+            onClick={() => setCurrentLink("projects")}
+            href="/projects"
+            className={`${rubik.className} absolute left-1/2 top-6 -translate-x-1/2 transform text-lg lg:hidden`}
+          >
+            <FaBuilding />
+          </Link>
+        </div>
 
+        <div className="relative h-full w-28">
+          <Link
+            onClick={() => setCurrentLink("resources")}
+            href="/resources"
+            className={`${rubik.className} absolute left-1/2 top-6 hidden -translate-x-1/2 transform text-lg lg:block`}
+          >
+            Resources
+          </Link>
+          <Link
+            title="Check additional resources"
+            onClick={() => setCurrentLink("resources")}
+            href="/resources"
+            className={`${rubik.className} absolute left-1/2 top-6 -translate-x-1/2 transform text-lg lg:hidden`}
+          >
+            <FaQuestion />
+          </Link>
+        </div>
 
-      <div className="relative h-full w-28">
-        <Link
-          onClick={() => setCurrentLink("resources")}
-          href="/resources"
-          className={`${rubik.className} absolute left-1/2 top-6 hidden -translate-x-1/2 transform text-lg lg:block`}
-        >
-          Resources
-        </Link>
-        <Link
-          title="Check additional resources"
-          onClick={() => setCurrentLink("resources")}
-          href="/resources"
-          className={`${rubik.className} absolute left-1/2 top-6 -translate-x-1/2 transform text-lg lg:hidden`}
-        >
-          <FaQuestion />
-        </Link>
-        {currentLink === "resources" && (
-          <div className="absolute -bottom-1 left-1/2 h-2 w-10 -translate-x-1/2 transform rounded-full bg-fuchsia-600"></div>
-        )}
+        {/* Single underline that slides between items. Each item is w-28 (7rem)
+            with a gap-x-8 (2rem), so the step between centers is 9rem. */}
+        <div
+          className="pointer-events-none absolute -bottom-1 left-0 h-2 w-10 rounded-full bg-fuchsia-600 transition-[transform,opacity] duration-300 ease-out motion-reduce:transition-none"
+          style={{
+            transform: `translateX(calc(${Math.max(activeIndex, 0)} * 9rem + 3.5rem - 50%))`,
+            opacity: activeIndex === -1 ? 0 : 1,
+          }}
+        />
       </div>
 
       {/* <div>
